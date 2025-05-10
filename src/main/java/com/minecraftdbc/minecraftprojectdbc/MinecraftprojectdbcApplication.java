d73e0ac16101e427b573473a4f1f10191ee4ad2c
Johnd3r <johndersanchez001@gmail.com>
2025-05-09 20:48:53 -0500

    IndexBuild

---
+++ src/main/java/com/minecraftdbc/minecraftprojectdbc/MinecraftprojectdbcApplication.java
@@ -0,0 +1,13 @@
+package com.minecraftdbc.minecraftprojectdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftprojectdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftprojectdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minecraftdbc/minecraftprojectdbc/MinecraftprojectdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minecraftdbc.minecraftprojectdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftprojectdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
